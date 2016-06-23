a441f2b12cfdc393d72d9fdb39fc2b89cb4be6ee
Anartz Mugika <mugan86@gmail.com>
2016-06-23 08:13:43 +0200

    Add constants file

---
+++ app/scripts/constants.js
@@ -0,0 +1,21 @@
+/**********************************************************************************************
+Constant values files
+----------------------------
+
+
+***********************************************************************************************/
+angular.module('apisApp')
+
+.constant('SERVIRACE_URL_LOCALHOST', 'http://mugan86.com/serviraces/api/v1/')
+.constant('WIKIPEDIA_MOBILE_WIKI_URL', 'https://en.m.wikipedia.org/wiki/')
+.constant('BASKETBALL_REFERENCE', 'http://www.basketball-reference.com')
+.constant('F1', {
+                        main_url: 'http://ergast.com/api/f1/',
+                        drivers: 'drivers.json',
+                        circuits: 'circuits.json'
+                    })
+.constant('REST_COUNTRIES', {
+                        main_url: 'http://restcountries.eu',
+                        all: '/rest/v1/all'
+                    })
+;
