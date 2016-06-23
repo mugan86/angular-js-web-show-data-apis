32d466a02793fa1faf35f4e275a4e15927850fde
Anartz Mugika <mugan86@gmail.com>
2016-06-23 08:21:57 +0200

    Add new csv files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -46,33 +46,4 @@ angular.module('apisApp')
 
       });
 
-
-
-    /*readCSV();
-
-    function readCSV () {
-      // http get request to read CSV file content
-      $http.get('/files/nba_history_roy.csv').success($scope.processData);
-    };
-
-    $scope.processData = function(allText) {
-      // split content based on new line
-      var allTextLines = allText.split(/\r\n|\n/);
-      var headers = allTextLines[0].split(',');
-      var lines = [];
-
-      for ( var i = 0; i < allTextLines.length; i++) {
-        // split content based on comma
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-          var tarr = [];
-          for ( var j = 0; j < headers.length; j++) {
-            tarr.push(data[j]);
-          }
-          lines.push(tarr);
-        }
-      }
-      $scope.data = lines;
-      console.log("Items: " + $scope.data.length);
-    };*/
-    });
+});
