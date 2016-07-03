f5fa104756658351ae140339441780a49867b0f6
anartzmugika <mugan86@gmail.com>
2016-07-03 08:11:26 +0200

    Add server files to make Spotify API request to get user token

---
+++ server/get_spotify_api_token.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'active_cors.php';
+
+//Get data from our Spotify profile: https://developer.spotify.com/my-applications/#!/applications/<CLIENT_ID>
+$client_id = '<CLIENT_ID>'; 
+$client_secret = '<CLIENt_SECRET>'; 
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,            'https://accounts.spotify.com/api/token' );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_POST,           1 );
+curl_setopt($ch, CURLOPT_POSTFIELDS,     'grant_type=client_credentials' ); 
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: Basic '.base64_encode($client_id.':'.$client_secret))); 
+
+$result=curl_exec($ch);
+echo $result;
+
+?>
